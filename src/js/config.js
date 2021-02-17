583f3ff34c98047188d8fd412c37f3b1dcb28692
Lasha-Giorgi <giorgilasha3@gmail.com>
2021-02-17 19:33:27 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '462efd3a-8ea5-4b3b-a4bd-4d99d3c5ab6a';
+export const MODAL_CLOSE_SEC = 2.5;
